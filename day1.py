23e9a8d07f1599d7152098d40209b2a8dfe0754e
Jean-Michel Carter <jeanmichel.carter@gmail.com>
2021-12-01 14:11:39 -0500

    Remove timer in the functions, since benchmarking done in pytest.

--- day1.py
+++ day1.py
@@ -1,7 +1,3 @@
-from utils import timer
-
-
-@timer
 def get_data():
     with open("inputs/day1.txt") as f:
         return [int(x) for x in f]
@@ -11,12 +7,10 @@ def count_increasing_adjacent_skip(data, skip):
     return len([None for x, y in zip(data, data[skip:]) if y > x])
 
 
-@timer
 def part1(data):
     return count_increasing_adjacent_skip(data, 1)
 
 
-@timer
 def part2(data):
     return count_increasing_adjacent_skip(data, 3)
 
