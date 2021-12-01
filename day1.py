326b62861c7c0889cde86d78cb6bb1f43c634360
Jean-Michel Carter <jeanmichel.carter@gmail.com>
2021-12-01 10:31:09 -0500

    Rename factored out function ... add pytest benchmark

--- day1.py
+++ day1.py
@@ -7,24 +7,41 @@ def get_data():
         return [int(x) for x in f]
 
 
-def len_increasing(r1, r2):
-    return len([None for x, y in zip(r1, r2) if y > x])
+def count_increasing_adjacent_skip(data, skip):
+    return len([None for x, y in zip(data, data[skip:]) if y > x])
 
 
 @timer
 def part1(data):
-    return len_increasing(data, data[1:])
+    return count_increasing_adjacent_skip(data, 1)
 
 
 @timer
 def part2(data):
-    return len_increasing(data, data[3:])
+    return count_increasing_adjacent_skip(data, 3)
 
 
 def main():
     data = get_data()
     print(part1(data))
     print(part2(data))
+
+
+def test_get_data_benchmark(benchmark):
+    data = benchmark(get_data)
+    assert len(data)
+
+
+def test_part1_benchmark(benchmark):
+    data = get_data()
+    result = benchmark(part1, data)
+    assert result == 1527
+
+
+def test_part2_benchmark(benchmark):
+    data = get_data()
+    result = benchmark(part2, data)
+    assert result == 1575
 
 
 if __name__ == "__main__":
