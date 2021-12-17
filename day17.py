0e9f454456d737461f0b8a5f86b8eef2be5e517f
Jean-Michel Carter <jeanmichel.carter@gmail.com>
2021-12-17 18:15:05 -0500

    refactored day17 to use generators, and a more functional style

--- day17.py
+++ day17.py
@@ -1,5 +1,6 @@
+from itertools import accumulate
 from math import sqrt, ceil
-from utils import timer
+from utils import timer, integers_from
 
 
 @timer
@@ -17,45 +18,51 @@ def part1(data):
     return max_height(-(data[1][0] + 1))
 
 
+def vx_gen(init):
+    while True:
+        yield init
+        init = max(0, init - 1)
+
+
+def vy_gen(init):
+    while True:
+        yield init
+        init -= 1
+
+
 def find_valid_times(vy, ty):
-    valid_times = []
     ymin, ymax = ty
-    t = 0
-    y = 0
-    while y >= ymin:
-        if ymin <= y <= ymax:
-            valid_times.append(t)
-        t += 1
-        y += vy
-        vy -= 1
-    return valid_times
+    ts = integers_from(1)
+    ys = accumulate(vy_gen(vy))
+
+    return (t for t, y, _ in zip(ts, ys, range(160)) if ymin <= y <= ymax)
 
 
 def find_vxmin(xmin):
     return ceil((sqrt(1 + 8 * xmin) - 1) / 2)
 
 
-def find_all_valid_vx0(vy, tx, ty):
-    valid_times = find_valid_times(vy, ty)
-    xmin, xmax = tx
-    vx0 = []
-    for t in valid_times:
-        for vx in range(find_vxmin(xmin), xmax + 1):
-            x = 0
-            v = vx
-            for _ in range(t):
-                x += v
-                v -= 1 if v else 0
-            if xmin <= x <= xmax:
-                vx0.append(vx)
+def last_x(t, vx):
+    *_, (x, _) = zip(accumulate(vx_gen(vx)), range(t))
+    return x
 
-    return vx0
+
+def find_all_valid_vx0(vy, tx, ty):
+    xmin, xmax = tx
+    return (
+        vx
+        for t in find_valid_times(vy, ty)
+        for vx in range(find_vxmin(xmin), xmax + 1)
+        if xmin <= last_x(t, vx) <= xmax
+    )
 
 
 @timer
 def part2(data):
     ymin = data[1][0]
-    init = set((vx, vy) for vy in range(ymin, -ymin) for vx in find_all_valid_vx0(vy, *data))
+    init = set(
+        (vx, vy) for vy in range(ymin, -ymin) for vx in find_all_valid_vx0(vy, *data)
+    )
 
     return len(init)
 
